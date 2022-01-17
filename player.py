f1fd56fc2cc429a75d0d03660f0a215dbcce5b39
YahiroRyo <honakafn@gmail.com>
2022-01-17 11:30:34 +0900

    init commit

---
+++ player.py
@@ -0,0 +1,63 @@
+import pygame
+from pygame.event import Event
+from pygame.surface import Surface
+from pygame.locals import *
+from map import Map
+import math
+
+# プレイヤー
+class Player:
+    img = None
+    x = 10
+    y = 10
+    speed = .05
+
+    def __init__(self, img) -> None:
+        self.img = img
+
+    def draw(self, screen: Surface) -> None:
+        screen.blit(self.img, (self.x, self.y))
+
+    def is_wall(self, idx: int):
+        walls = [2]
+        is_wall = False
+        for wall in walls:
+            if wall == idx:
+                is_wall = True
+        return is_wall
+
+    def event(self, map: Map):
+        keys = pygame.key.get_pressed()
+        (idx, wx, wy) = self.get_block(map)
+        is_wall = self.is_wall(idx)
+        if keys[K_UP]:
+            self.y -= self.speed
+            if is_wall:
+                self.y += self.speed + 1
+        if keys[K_DOWN]:
+            self.y += self.speed
+            if is_wall:
+                self.y -= self.speed + 1
+        if keys[K_LEFT]:
+            self.x -= self.speed
+            if is_wall:
+                self.x += self.speed + 1
+        if keys[K_RIGHT]:
+            self.x += self.speed
+            if is_wall:
+                self.x -= self.speed + 1
+
+    def get_block(self, map: Map):
+        return (map.map[math.floor(self.y / map.msize)][math.floor(self.x / map.msize)], math.floor(self.x / map.msize) * map.msize, math.floor(self.y / map.msize) * map.msize)
+
+    def proc(self, map: Map, scene, scenes):
+        (idx, wx, wy) = self.get_block(map)
+        if idx == 0:
+            # 草
+            pass
+        elif idx == 1:
+            # 水
+            pass
+        for k in scene.conf:
+            if idx == k:
+                scenes.current_scene = scene.conf[k]
---
+++ scene.py
@@ -0,0 +1,64 @@
+import pygame
+from pygame.event import Event
+from pygame.surface import Surface
+from pygame.locals import *
+from player import Player
+from map import Map
+import sys
+
+print(len(sys.argv))
+SW = 640 if len(sys.argv) == 1 else int(sys.argv[1])
+SH = 480 if len(sys.argv) == 1 else int(sys.argv[2])
+SCR_RECT = Rect(0, 0, SW, SH)  # 画面サイズ
+
+def load_img(filename, colorkey=None):
+    img = pygame.image.load(filename)
+    img = img.convert()
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = img.get_at((0, 0))
+        img.set_colorkey(colorkey, RLEACCEL)
+    return img
+
+
+class Scene:
+    map: Map
+    name: str = ""
+    conf: dict = {}
+
+    def __init__(self, map: Map, name: str, conf: dict = {}) -> None:
+        self.map = map
+        self.name = name
+        self.conf = conf
+
+class Scenes:
+    scenes: list = []
+    current_scene = 0
+
+    def set_scene(self, scene: Scene):
+        self.scenes.append(scene)
+
+    def start(self):
+        pygame.init()
+        screen = pygame.display.set_mode(SCR_RECT.size)
+        player = Player(load_img("imgs/man.png"))
+        Map.imgs[0] = load_img("imgs/grass.png")
+        Map.imgs[1] = load_img("imgs/water.png")
+        Map.imgs[2] = load_img("imgs/wall.png")
+        Map.imgs[10] = load_img("imgs/wall.png")
+
+        while True:
+            player.event(self.scenes[self.current_scene].map)
+            player.proc(self.scenes[self.current_scene].map, self.scenes[self.current_scene], self)
+            self.scenes[self.current_scene].map.draw(screen)
+            player.draw(screen)
+            pygame.display.update()
+            for event in pygame.event.get():
+                # 終了用のイベント処理
+                if event.type == QUIT:          # 閉じるボタンが押されたとき
+                    pygame.quit()
+                    sys.exit()
+                if event.type == KEYDOWN:       # キーを押したとき
+                    if event.key == K_ESCAPE:   # Escキーが押されたとき
+                        pygame.quit()
+                        sys.exit()
