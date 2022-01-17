08777c3bf233c0f113cc1e473f4db2230a5fd6fa
YahiroRyo <honakafn@gmail.com>
2022-01-17 20:47:27 +0900

    mapに配置XとYを定義

--- map.py
+++ map.py
@@ -1,4 +1,3 @@
-from tkinter import SW
 import scene
 
 class Map:
@@ -7,17 +6,21 @@ class Map:
     row, col = 0, 0  # マップの行数,列数を取得
     imgs = [None] * 256             # マップチップ
     msize = 32                      # 1マスの大きさ[px]
+    x = 0                           # マップ配置始点X
+    y = 0                           # マップ配置始点Y
     
     def __init__(self, map: list) -> None:
         self.map = map
         self.row = len(map)
         self.col = len(map[0])
+        self.x = scene.SW / 2 - self.col * self.msize / 2
+        self.y = scene.SH / 2 - self.row * self.msize / 2
 
     # マップの描画
     def draw(self, screen):
-        x_set=scene.SW/2-self.col*self.msize/2
-        y_set=scene.SH/2-self.row*self.msize/2
         for i in range(self.row):
             for j in range(self.col):
-                screen.blit(self.imgs[self.map[i][j]],
-                            (j*self.msize+x_set, i*self.msize+y_set))
+                screen.blit(
+                    self.imgs[self.map[i][j]],
+                    (j * self.msize + self.x, i * self.msize + self.y)
+                )
