bd4c5bee9ea533c0679f3e84e78f1f4cd99bbab0
yossi3021 <unitamilucky777@gmail.com>
2022-01-17 20:18:39 +0900

    マップを中央ぞろえ表示に変更※判定はずれている…

--- map.py
+++ map.py
@@ -1,3 +1,4 @@
+from tkinter import SW
 import scene
 
 class Map:
@@ -14,7 +15,11 @@ class Map:
 
     # マップの描画
     def draw(self, screen):
+        x_set=scene.SW/2-self.col*self.msize/2
+        y_set=scene.SH/2-self.row*self.msize/2
+        print(x_set)
+        print(y_set)
         for i in range(self.row):
             for j in range(self.col):
                 screen.blit(self.imgs[self.map[i][j]],
-                            (j*self.msize, i*self.msize))
+                            (j*self.msize+x_set, i*self.msize+y_set))
