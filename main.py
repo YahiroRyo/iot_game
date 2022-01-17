89b9347a2a916dce35cb2a01e3f55755ec3e3774
HayatoMurakami73 <sadoucunshang@gmail.com>
2022-01-17 14:24:50 +0900

    インストールのバグ修正

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import scene
-import mapdata
 import subprocess
 import os
+subprocess.run(f"cd {os.getcwd()} && pip install -r requirements.txt", shell=True)
+import scene
+import mapdata
+
+.
 
 if __name__ == "__main__":
-    subprocess.run(f"cd {os.getcwd()} && pip install -r requirements.txt", shell=True)
     scenes = scene.Scenes()
 
     scenes.set_scene(scene.Scene(mapdata.map_1, "平原", {
