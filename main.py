658102cb48edd7a213499312a7d9315c3ade9fe4
yappi <76673979+YahiroRyo@users.noreply.github.com>
2022-01-17 12:57:22 +0900

    番号から名称に変更

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ if __name__ == "__main__":
     scenes = scene.Scenes()
 
     scenes.set_scene(scene.Scene(mapdata.map_1, "平原", {
-        10: 1
+        10: "例"
     }))
     scenes.set_scene(scene.Scene(mapdata.map_2, "例", {
-        10: 0
+        10: "平原"
     }))
     scenes.start()
