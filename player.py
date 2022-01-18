e4530cc7fa16ae7319b5cfad5db6416a32350fb9
YahiroRyo <honakafn@gmail.com>
2022-01-18 10:42:15 +0900

    壁の当たり判定をキャラクターの幅に

--- player.py
+++ player.py
@@ -1,3 +1,4 @@
+import time
 import pygame
 from pygame.event import Event
 from pygame.surface import Surface
@@ -10,6 +11,7 @@ class Player:
     img = None
     x = 32
     y = 32
+    size = 30
     speed = .05
 
     def __init__(self, img) -> None:
@@ -26,47 +28,77 @@ class Player:
                 is_wall = True
         return is_wall
 
-    def key_is_wall(self, key: int, map: Map):
+    def get_positions(self, key: int):
         x = 0
         y = 0
+        tmp_x = 0
+        tmp_y = 0
+        
         if key == K_UP:
-            y = -self.speed
+            y -= self.speed
+            tmp_x = self.size
         if key == K_DOWN:
-            y = self.speed
+            y += self.size + self.speed
+            tmp_x = self.size
         if key == K_LEFT:
-            x = -self.speed
+            x -= self.speed
+            tmp_y = self.size
         if key == K_RIGHT:
-            x = self.speed
-        id = map.map[math.floor((self.y + y) / map.msize)][math.floor((self.x + x) / map.msize)]
-        return self.is_wall(id)
+            x += self.size + self.speed
+            tmp_y = self.size
+        return (x, y, tmp_x, tmp_y)
+
+    def get_keys(self):
+        ret: list = []
+        keys = pygame.key.get_pressed()
+        if keys[K_UP]:
+           ret.append(K_UP) 
+        if keys[K_DOWN]:
+           ret.append(K_DOWN) 
+        if keys[K_LEFT]:
+           ret.append(K_LEFT) 
+        if keys[K_RIGHT]:
+           ret.append(K_RIGHT) 
+        return ret
+    
+    def key_is_wall(self, key: int, map: Map):
+        (x, y, tmp_x, tmp_y) = self.get_positions(key)
+        return  self.is_wall(self.get_block(map, self.x + x, self.y + y)) or self.is_wall(self.get_block(map, self.x + tmp_x + x, self.y + tmp_y + y))
 
     def event(self, map: Map):
         keys = pygame.key.get_pressed()
         if keys[K_UP]:
             if not self.key_is_wall(K_UP, map) and self.y > 0: self.y -= self.speed
         if keys[K_DOWN]:
-            if not self.key_is_wall(K_DOWN, map) and self.y < map.row * map.msize - 31: self.y += self.speed
+            if not self.key_is_wall(K_DOWN, map) and self.y < map.row * map.msize - self.size: self.y += self.speed
         if keys[K_LEFT]:
             if not self.key_is_wall(K_LEFT, map) and self.x > 0: self.x -= self.speed
         if keys[K_RIGHT]:
-            if not self.key_is_wall(K_RIGHT, map) and self.x < map.col * map.msize - 31: self.x += self.speed
+            if not self.key_is_wall(K_RIGHT, map) and self.x < map.col * map.msize - self.size: self.x += self.speed
 
-    def get_block(self, map: Map):
-        return (map.map[math.floor(self.y / map.msize)][math.floor(self.x / map.msize)], math.floor(self.x / map.msize) * map.msize, math.floor(self.y / map.msize) * map.msize)
+    def get_block(self, map: Map, x: int = -1, y: int = -1):
+        tmp_x = self.x if x == -1 else x
+        tmp_y = self.y if y == -1 else y
+        return map.map[math.floor(tmp_y / map.msize)][math.floor(tmp_x / map.msize)]
 
     def proc(self, map: Map, scene, scenes):
-        (idx, wx, wy) = self.get_block(map)
-        if idx == 0:
-            # 草
-            pass
-        elif idx == 1:
-            # 水
-            pass
-        for k in scene.conf:
-            if idx == k:
-                for i, v in enumerate(scenes.scenes):
-                    if v.name == scene.conf[k][0]:
-                        scenes.current_scene = i
-                        self.x = scene.conf[k][1]
-                        self.y = scene.conf[k][2]
-                        return
+        keys = self.get_keys()
+        for key in keys:
+            (x, y, tmp_x, tmp_y) = self.get_positions(key)
+            self_pos = self.get_block(map, self.x + x, self.y + y)
+            will_pos = self.get_block(map, self.x + tmp_x, self.y + tmp_y)
+            if self_pos == 0:
+                # 草
+                pass
+            elif self_pos == 1:
+                # 水
+                pass
+            for k in scene.conf:
+                if self_pos == k or will_pos == k:
+                    for i, v in enumerate(scenes.scenes):
+                        if v.name == scene.conf[k][0]:
+                            scenes.current_scene = i
+                            self.x = scene.conf[k][1]
+                            self.y = scene.conf[k][2]
+                            time.sleep(1)
+                            return
