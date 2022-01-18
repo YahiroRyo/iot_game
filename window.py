0df97fdeadae80c50eff3e72fd006ff04e98d841
yossi3021 <unitamilucky777@gmail.com>
2022-01-18 12:32:39 +0900

    color.pyを追加

--- window.py
+++ window.py
@@ -1,8 +1,6 @@
 from pygame.surface import Surface
 import pygame
-
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+import color
 
 class Window:
     _w = 0
@@ -11,14 +9,14 @@ class Window:
     _y = 0
     
     def _draw(self, screen: Surface, width: int, height: int, x: int, y: int):
-        pygame.draw.rect(screen, WHITE, (x, y, width, height), 5)
-        pygame.draw.rect(screen, BLACK, (x + 5, y + 5, width - 10, height - 10))
+        pygame.draw.rect(screen, color.WHITE, (x, y, width, height), 5)
+        pygame.draw.rect(screen, color.BLACK, (x + 5, y + 5, width - 10, height - 10))
         self._w = width
         self._h = height
         self._x = x
         self._y = y
 
-    def _draw_str(self, screen: Surface, string: str, color = WHITE, font_size: int = 32):
+    def _draw_str(self, screen: Surface, string: str, color = color.ORANGE, font_size: int = 32):
         font = pygame.font.SysFont(None, font_size)
         text = font.render(string, True, color)
         screen.blit(text, (self._x + 10, self._y + 10))
