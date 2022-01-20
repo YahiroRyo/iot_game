c1a5cfe88a45ad12969baf26f80a589ac5895cc8
yossi3021 <unitamilucky777@gmail.com>
2022-01-20 15:10:47 +0900

    エンカウントした際モンスターをランダムで取得

---
+++ monsterdata.py
@@ -0,0 +1,16 @@
+#    img=""              #モンスター画像
+#    monster_name=""     #モンスターの名前
+#    hp=0
+#    mp=0
+#    power=0             #物理攻撃
+#    M_power=0           #魔法攻撃
+#    defense=0           #物理防御
+#    M_defense=0         #魔法防御
+#    agility=0           #素早さ
+#    luck=0              #会心率
+#    attack_times=1      #行動回数
+
+
+#          img          名前       hp   mp 攻 魔 防 ﾏ防 早 会 行動回数 
+suraimu=["suraimu.png", "スライム", 20, 0, 3, 0, 1, 1, 5, 0, 1]
+goburin=["goburin.png", "ゴブリン", 30, 0, 5, 0, 2, 1, 2, 1, 1]
