bcd11e6cce6ad86a42ddebd1248759364a4dda77
yossi3021 <unitamilucky777@gmail.com>
2022-01-22 23:14:53 +0900

    画面サイズを1280*720に変更

--- monster.py
+++ monster.py
@@ -7,9 +7,9 @@ class Monster:
     hp=0
     mp=0
     power=0             #物理攻撃
-    M_power=0           #魔法攻撃
+    m_power=0           #魔法攻撃
     defense=0           #物理防御
-    M_defense=0         #魔法防御
+    m_defense=0         #魔法防御
     agility=0           #素早さ
     luck=0              #会心率
     attack_times=1      #行動回数
@@ -33,20 +33,13 @@ class Monster:
         self.hp=self.random(data[2])
         self.mp=self.random(data[3])
         self.power=self.random(data[4])
-        self.M_power=self.random(data[5])
+        self.m_power=self.random(data[5])
         self.defense=self.random(data[6])
-        self.M_defense=self.random(data[7])
+        self.m_defense=self.random(data[7])
         self.agility=self.random(data[8])
         self.luck=self.random(data[9])
         self.attack_times=self.random(data[10])
-        print(self.img)
-        print(self.hp)
-        print(self.monster_name)
-        print(self.mp)
-        print(self.power)
-        print(self.M_power)
-        print(self.defense)
-        print(self.M_defense)
-        print(self.agility)
-        print(self.luck)
-        print(self.attack_times)
+        self.debug()
+
+    def debug(self):
+        print(f"{self.img}, {self.monster_name}, {self.hp}, {self.mp}, {self.power}, {self.m_power}, {self.defense}, {self.m_defense}, {self.agility}, {self.luck}, {self.attack_times}")
