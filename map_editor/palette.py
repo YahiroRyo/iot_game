57ef706137e5d16963a566703cf57b3cec9fe723
YahiroRyo <honakafn@gmail.com>
2022-02-11 08:45:23 +0900

    パレットの改行を追加

--- map_editor/palette.py
+++ map_editor/palette.py
@@ -8,9 +8,11 @@ class Palette(KeyEvent, Canvas):
     def __init__(self, context: Context):
         super().__init__(context)
         self.set_canvas((0, 0), (context.get("SW") / 3, context.get("SH")), (255, 255, 255))
+        sum_idx = 0
         for (idx, block) in enumerate(BLOCKS):
-            block.x = idx * 40
-            block.y = 0
+            block.x = (idx - sum_idx) * 40
+            block.y = int((idx * 40) / (context.get("SW") / 3.2)) * 40
+            sum_idx += int((context.get("SW") / 2.9) / 40) if int(((idx - sum_idx) * 40) / (context.get("SW") / 3.2)) else 0
 
     def mouse_down_left(self, context: Context):
         (x, y) = pygame.mouse.get_pos()
